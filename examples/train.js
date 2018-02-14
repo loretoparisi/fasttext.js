893387c8486dc3a61b9e95a20821c917db0ac383
Loreto Parisi <loretoparisi@gmail.com>
2018-02-14 12:57:35 +0100

    added more env parameters for train example

--- examples/train.js
+++ examples/train.js
@@ -6,57 +6,61 @@
 
 "use strict";
 
-(function() {
+(function () {
 
-var DATA_ROOT=__dirname+'/data';
-var FastText = require('../lib/index');
+    var DATA_ROOT = __dirname + '/data';
 
-var fastText = new FastText({
-    debug: true,
-    train: {
-        // number of concurrent threads
-        thread: 8,
-        // verbosity level [2]
-        verbose: 4,
-        // number of negatives sampled [5]
-        neg: 5,
-        // loss function {ns, hs, softmax} [ns]
-        loss: 'ns',
-        // learning rate [0.05]
-        lr: process.env.TRAIN_LR || 0.05,
-        // change the rate of updates for the learning rate [100]
-        lrUpdateRate: 100,
-        // max length of word ngram [1]
-        wordNgrams: process.env.TRAIN_NGRAM || 1,
-        // minimal number of word occurences
-        minCount: 1,
-        // minimal number of word occurences
-        minCountLabel: 1,
-        // size of word vectors [100]
-        dim: process.env.TRAIN_DIM || 100,
-        // size of the context window [5]
-        ws: process.env.TRAIN_WS || 5,
-        //  number of epochs [5]
-        epoch: process.env.TRAIN_EPOCH || 5,
-        // number of buckets [2000000]
-        bucket: process.env.TRAIN_BUCKET || 2000000,
-        // min length of char ngram [3]
-        minn: 3,
-        // max length of char ngram [6]
-        maxn: 6,
-        // sampling threshold [0.0001]
-        t: 0.0001
-    },
-    serializeTo: DATA_ROOT + '/band_model', // do not specify ext: 'bin' will be added
-    trainFile: DATA_ROOT + '/band_train.txt'
-});
+    var TRAINFILE = process.env.TRAINFILE || DATA_ROOT + '/band_train.txt';
+    var SERIALIZETO = process.env.SERIALIZETO || DATA_ROOT + '/band_model'; // do not specify ext: 'bin' will be added
 
-fastText.train()
-.then(done=> {
-    console.log("train done.");
-})
-.catch(error => {
-    console.error("train error",error);
-})
+    var FastText = require('../lib/index');
+
+    var fastText = new FastText({
+        debug: true,
+        train: {
+            // number of concurrent threads
+            thread: 8,
+            // verbosity level [2]
+            verbose: 4,
+            // number of negatives sampled [5]
+            neg: 5,
+            // loss function {ns, hs, softmax} [ns]
+            loss: process.env.TRAIN_LOSS || 'ns',
+            // learning rate [0.05]
+            lr: process.env.TRAIN_LR || 0.05,
+            // change the rate of updates for the learning rate [100]
+            lrUpdateRate: 100,
+            // max length of word ngram [1]
+            wordNgrams: process.env.TRAIN_NGRAM || 1,
+            // minimal number of word occurences
+            minCount: 1,
+            // minimal number of word occurences
+            minCountLabel: 1,
+            // size of word vectors [100]
+            dim: process.env.TRAIN_DIM || 100,
+            // size of the context window [5]
+            ws: process.env.TRAIN_WS || 5,
+            //  number of epochs [5]
+            epoch: process.env.TRAIN_EPOCH || 5,
+            // number of buckets [2000000]
+            bucket: process.env.TRAIN_BUCKET || 2000000,
+            // min length of char ngram [3]
+            minn: process.env.TRAIN_MINN || 2,
+            // max length of char ngram [6]
+            maxn: process.env.TRAIN_MAXN || 4,
+            // sampling threshold [0.0001]
+            t: 0.0001
+        },
+        serializeTo: SERIALIZETO,
+        trainFile: TRAINFILE
+    });
+
+    fastText.train()
+        .then(done => {
+            console.log("train done.");
+        })
+        .catch(error => {
+            console.error("train error", error);
+        })
 
 }).call(this);
