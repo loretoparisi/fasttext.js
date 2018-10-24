d8ae2ff093f335399847c903d3e692e76a3ddaa6
Loreto Parisi <loretoparisi@gmail.com>
2018-10-24 18:17:46 +0200

    Added testLabels to support new parameter test-label for F1, Accuracy, Precision by label

--- examples/testlabels.js
+++ examples/testlabels.js
@@ -27,7 +27,7 @@ var fastText = new FastText({
 fastText.testLabels()
 .then(evaluation=> {
     console.log("labels:", fastText.getLabels());
-    console.log("test-labels:",JSON.stringift(evaluation,null,2));
+    console.log("test-labels:",JSON.stringify(evaluation,null,2));
 })
 .catch(error => {
     console.error("test error",error);
