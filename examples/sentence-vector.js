350c6de4295d609d9650f13959d83454227f42a9
Mariano Andres Botta <marianoabotta@gmail.com>
2020-04-24 22:58:00 -0300

    Add print-sentence-vectors feature

---
+++ examples/sentence-vector.js
@@ -0,0 +1,39 @@
+/**
+ * FastText.js
+ * @author Loreto Parisi (loretoparisi at gmail dot com)
+ * @copyright Copyright (c) 2017-2019 Loreto Parisi
+*/
+
+"use strict";
+
+(function () {
+
+    var MODELS_ROOT = __dirname + '/models';
+
+    var FastText = require('../lib/index');
+    var ft = new FastText({
+        loadModel: MODELS_ROOT + '/sms_model.bin' // must specifiy filename and ext
+    });
+
+    var sample = "You have won a phone! Please apply now!";
+    ft.loadSentence()
+        .then(done => {
+            return ft.sentence(sample);
+        })
+        .then(vector => {
+            console.log("TEXT:", sample, "\nPREDICT:", vector);
+            sample = "Hi John this is Scott, please call me back";
+            return ft.sentence(sample);
+        })
+        .then(vector => {
+            console.log("TEXT:", sample, "\nPREDICT:", vector);
+            ft.unload();
+        })
+        .then(done => {
+            console.log("model unloaded.");
+        })
+        .catch(error => {
+            console.error("predict error", error);
+        });
+
+}).call(this);
